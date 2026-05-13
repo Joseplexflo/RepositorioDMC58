a27ff2e8a27f42ea7092e687f29d2864beddd11b
Joseplexflo <jlezmaf@gmail.com>
2026-05-12 19:47:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.title("Mi primera aplicación en Python")
 st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Jhonattan Lezma F.")
+
+sesion = st.selecbox("Seleccione una sessión", ["Sessión 1","Sessión 2","Sessión 3","Sessión 4"])
