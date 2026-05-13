02ecf492a20c9958a77207e16912397749213bfd
Joseplexflo <jlezmaf@gmail.com>
2026-05-12 19:55:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,18 @@ st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Jhonattan Lezma F.")
 
-sesion = st.sidebar.selectbox("Seleccione una sessión", ["Sessión 1","Sessión 2","Sessión 3","Sessión 4"])
+sesion = st.sidebar.selectbox("Seleccione una sessión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"])
+
+if sesion == "Sesión 1":
+  st.write("Bienvenido a la Sesión 1")
+  
+elif sesion == "Sesión 2":
+  st.write("Bienvenido a la Sesión 2")
+  
+elif sesion == "Sesión 3":
+  st.write("Bienvenido a la Sesión 3")
+  
+else sesion == "Sesión 4":
+  st.write("Bienvenido a la Sesión 4")
+
+
